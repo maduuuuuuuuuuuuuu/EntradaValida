b1f036459b0e6c76e986ba2e845aba6dd2c8f883
maduuuuuuuuuuuuuu <e107ti@etec.sp.gov.br>
2023-11-24 20:36:14 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Entrada Válida");
+int numero = -1;
+
+  while (numero < 0|| numero > 9)
+{
+    Console.Write("Digite um número entre 1 e 9: ");
+    Int32.TryParse(Console.ReadLine(), out numero);
+
+}
+
+if(numero == 0){
+    Console.WriteLine("Operação Cancelada");
+}
+else{
+    Console.WriteLine($"Numero selecionado = {numero}");
+}
